8d3c7b9e8b587a3e3374bc1770866b8654d21297
Eloy Moreno <eloymoga@gmail.com>
2025-09-13 00:25:07 +0200

    fix

--- custom_components/llama_conversation/const.py
+++ custom_components/llama_conversation/const.py
@@ -146,9 +146,9 @@ PROMPT_TEMPLATE_ZEPHYR3 = "zephyr3"
 DEFAULT_PROMPT_TEMPLATE = PROMPT_TEMPLATE_CHATML
 PROMPT_TEMPLATE_DESCRIPTIONS = {
     PROMPT_TEMPLATE_GPT_OSS: {
-        "system": { "prefix": "<|start|>system<|message|>\n", "suffix": "<|end|>" },
-        "user": { "prefix": "<|start|>user<|message|>\n", "suffix": "<|end|>" },
-        "assistant": { "prefix": "<|start|>assistant<|channel|>final<|message|>\n", "suffix": "<|end|>" },
+        "system": { "prefix": "<|start|>system<|message|>", "suffix": "<|end|>" },
+        "user": { "prefix": "<|start|>user<|message|>", "suffix": "<|end|>" },
+        "assistant": { "prefix": "<|start|>assistant<|channel|>final<|message|>", "suffix": "<|end|>" },
         "tool": { "prefix": "<|start|>tool", "suffix": "<|end|>" },
         "chain_of_thought": { "prefix": "<|start|>assistant<|channel|>analysis<|message|>", "suffix": "<|end|>"},
         "generation_prompt": "<|start|>assistant"
